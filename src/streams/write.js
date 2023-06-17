ea7da9b5755f65b34885e986fe53fdff57defbce
trenkenshu <trenkenshu@gmail.com>
2023-06-17 17:56:20 +0300

    feat: streams/write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,12 @@
-const write = async () => {
-    // Write your code here 
-};
+import { createWriteStream } from 'fs'
+import path from 'path'
 
-await write();
+const pathToFile = path.join(...process.argv[1].split(path.sep).slice(0, -1), 'files', 'fileToWrite.txt')
+
+const write = async () => {
+    const writeStream = createWriteStream(pathToFile)
+    console.log('To exit press Ctrl+C')
+    process.stdin.pipe(writeStream)
+}
+
+await write()
