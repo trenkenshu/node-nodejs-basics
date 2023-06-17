4765a6056bc604a62d8c24aa70c70fab2a280da2
trenkenshu <trenkenshu@gmail.com>
2023-06-17 09:17:38 +0300

    feat: args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
-};
+    const args = process.argv.slice(2)
+    const answer = []
+    args.forEach(item => {
+        if(/^--/.test(item)) {
+            answer.push(item.slice(2))
+        } else {
+            answer[answer.length - 1] += (` is ${item}`)
+        }
+    })
+    answer.length && console.log(answer.join(', '))
+}
 
-parseArgs();
+parseArgs()
