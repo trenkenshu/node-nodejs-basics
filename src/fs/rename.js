3f8319000c6470dcdbe345eeb9d0957e9908d146
trenkenshu <trenkenshu@gmail.com>
2023-06-17 08:26:23 +0300

    feat: rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,30 @@
-const rename = async () => {
-    // Write your code here 
-};
+import * as fs from 'node:fs/promises'
+import * as path from 'path'
 
-await rename();
+const initPath = path.join(...process.argv[1].split(path.sep).slice(0, -1), 'files');
+const sourcePath = path.join(initPath, 'wrongFilename.txt')
+const destinationPath = path.join(initPath, 'properFilename.md')
+
+const areWeOk = async () => {
+    try {
+        await fs.readFile(sourcePath)
+    } catch {
+        throw Error('FS operation failed')
+    }
+    try {
+        await fs.readFile(destinationPath)
+        throw Error('FS operation failed')
+    } catch {}
+
+    return
+}
+
+const rename = async () => {
+    await areWeOk();
+    fs.rename(
+        sourcePath,
+        destinationPath
+    )
+}
+
+await rename()
