5ed895a8eeed83b12601c64daf36d3350bf72ab0
trenkenshu <trenkenshu@gmail.com>
2023-06-17 08:36:46 +0300

    feat: list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,14 @@
-const list = async () => {
-    // Write your code here 
-};
+import * as fs from 'node:fs/promises'
+import * as path from 'path'
 
-await list();
+const initPath = path.join(...process.argv[1].split(path.sep).slice(0, -1), 'files');
+
+const list = async () => {
+    try {
+        console.table(await fs.readdir(initPath))
+    } catch {
+        throw Error('FS operation failed')
+    }
+}
+
+await list()
