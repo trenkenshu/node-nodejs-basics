89103f2a9ed28bd9df294941baf0a9388d59dc36
trenkenshu <trenkenshu@gmail.com>
2023-06-17 08:09:41 +0300

    feat: copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,30 @@
-const copy = async () => {
-    // Write your code here 
-};
+import * as fs from 'node:fs/promises';
+import * as path from 'path';
 
-await copy();
+const initPath = process.argv[1].split(path.sep).slice(0, -1)
+const sourcePath = path.join(...initPath, 'files')
+const destinationPath = path.join(...initPath, 'files_copy')
+
+const areWeOk = async () => {
+    try {
+        await fs.readdir(sourcePath)
+         await fs.mkdir(destinationPath);
+    } catch {
+        throw Error('FS operation failed')
+    }
+
+    return
+}
+
+const copy = async () => {
+    await areWeOk()
+    const files = fs.readdir(sourcePath);
+    (await files).forEach(file => {
+        fs.copyFile(
+            path.join(sourcePath, file),
+            path.join(destinationPath, file)
+        )
+    })
+}
+
+await copy()
