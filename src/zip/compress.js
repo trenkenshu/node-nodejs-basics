a80f85555e7c02f99bac024bc51b1951fe49017e
trenkenshu <trenkenshu@gmail.com>
2023-06-17 20:06:48 +0300

    feat: zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
-const compress = async () => {
-    // Write your code here 
-};
+import zlib from 'zlib'
+import { createReadStream, createWriteStream } from 'fs'
+import path from 'path'
 
-await compress();
+const fromPath = path.join(...process.argv[1].split(path.sep).slice(0, -1), 'files', 'fileToCompress.txt')
+const toPath = path.join(...process.argv[1].split(path.sep).slice(0, -1), 'files', 'archive.gz')
+
+const compress = async () => {
+    const from = createReadStream(fromPath)
+    const to = createWriteStream(toPath)
+    const gz = zlib.createGzip()
+
+    from.pipe(gz)
+        .pipe(to)
+}
+
+await compress()
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,17 @@
-const decompress = async () => {
-    // Write your code here 
-};
+import zlib from 'zlib'
+import { createReadStream, createWriteStream } from 'fs'
+import path from 'path'
 
-await decompress();
+const fromPath = path.join(...process.argv[1].split(path.sep).slice(0, -1), 'files', 'archive.gz')
+const unzippedPath = path.join(...process.argv[1].split(path.sep).slice(0, -1), 'files', 'fileToCompress.txt')
+
+const decompress = async () => {
+    const from = createReadStream(fromPath)
+    const to = createWriteStream(unzippedPath)
+    const gunz = zlib.createGunzip()
+
+    from.pipe(gunz)
+        .pipe(to)
+}
+
+await decompress()
