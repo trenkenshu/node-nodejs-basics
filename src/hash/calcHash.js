4c38a58a4124cfccc263b364d6712ff5721ff406
trenkenshu <trenkenshu@gmail.com>
2023-06-17 17:34:17 +0300

    feat: calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import { createHash } from 'crypto'
+import fs from 'fs/promises'
+import path from 'path'
 
-await calculateHash();
+const pathToFile = path.join(...process.argv[1].split(path.sep).slice(0, -1), 'files', 'fileToCalculateHashFor.txt')
+const calculateHash = async () => {
+    let content = '';
+    try {
+        content = await fs.readFile(pathToFile)
+    } catch {
+        throw Error('FS operation failed')
+    }
+    console.log(createHash('sha256').update(content).digest('hex'))
+}
+
+await calculateHash()
