ac65f4db30f7332fbe5f3fd4aeabdc927e212ad4
trenkenshu <trenkenshu@gmail.com>
2023-06-17 07:28:39 +0300

    feat: create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,12 @@
-const create = async () => {
-    // Write your code here 
-};
+import * as fs from 'node:fs/promises';
+import * as path from 'path';
 
-await create();
+const create = async () => {
+    const filePath = path.join(...process.argv[1].split(path.sep).slice(0, -1), 'files')
+    const isThere = (await fs.readdir(filePath)).includes('fresh.txt')
+    if(isThere)
+        throw Error('FS operation failed')
+    fs.writeFile(path.join(filePath, 'fresh.txt'), 'I am fresh and young')
+}
+
+await create()
