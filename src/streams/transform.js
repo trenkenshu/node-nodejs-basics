97e263b8f9b2a291f60dcec9cabe4fd9e27fab31
trenkenshu <trenkenshu@gmail.com>
2023-06-17 19:45:44 +0300

    feat: streams/transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
-const transform = async () => {
-    // Write your code here 
-};
+import { Transform } from 'stream'
 
-await transform();
+const transform = async () => {
+    const reverse = new Transform({
+        transform(data, encoding, cb) {
+            cb(null, String(data).split('').reverse().join(''));
+        },
+    })
+    process.stdin
+        .pipe(reverse)
+        .pipe(process.stdout)
+}
+
+await transform()
