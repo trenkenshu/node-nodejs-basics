c16fefb237d22d39ef46211cba4504d299a94b5e
trenkenshu <trenkenshu@gmail.com>
2023-06-18 15:51:37 +0300

    feat: cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,16 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+import CP from 'child_process'
+import { join, sep } from 'path'
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+const scriptPath = join(...process.argv[1].split(sep).slice(0, -1), 'files', 'script.js')
+
+const spawnChildProcess = async (args) => {
+    const cpArgs = args ? args : []
+    const childProc =  CP.execFile('node', [scriptPath, ...cpArgs], (err) =>
+        console.log(`Child process terminated ${err ? 'with error: ' + JSON.stringify(err): 'gracefully'}`)
+    )
+    console.log('Spawned child process id:', childProc.pid)
+    process.stdin.pipe(childProc.stdin)
+    childProc.stdout.pipe(process.stdout)
+}
+
+spawnChildProcess(['someArgument1', 'someArgument2', '3'])
