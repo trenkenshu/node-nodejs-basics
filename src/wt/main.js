c5cd75d75eb2610fc30ea63fe2497ff587fd87b7
trenkenshu <trenkenshu@gmail.com>
2023-06-18 14:09:49 +0300

    feat: wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,31 @@
-const performCalculations = async () => {
-    // Write your code here
-};
+import { Worker } from 'worker_threads'
+import { cpus } from 'os'
+import { join, sep } from 'path'
 
-await performCalculations();
+const workerPath = join(...process.argv[1].split(sep).slice(0, -1), 'worker.js');
+
+const performCalculations = async () => {
+    const cpusCount = cpus().length
+    const answers = [];
+    let finished = 0;
+    for (let i = 0; i < cpusCount; i++ ) {
+        const worker = new Worker(workerPath, { workerData: { n: i + 10 } })
+        worker.on(
+            'message',
+            data => answers[i] = { data, status: 'resolved' }
+        )
+        worker.on(
+            'error',
+            () => answers[i] = { data: null, status: 'error' }
+        )
+        worker.on(
+            'exit',
+            () => {
+                finished++
+                finished === cpusCount && console.log(answers)
+            }
+        )
+    }
+}
+
+await performCalculations()
